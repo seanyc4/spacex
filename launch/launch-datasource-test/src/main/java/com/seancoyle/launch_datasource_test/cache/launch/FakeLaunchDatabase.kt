c7dc0070366b007db08d203869f07be0a105eca5
asos-seancoyle <sean.coyle@asos.com>
2022-09-25 15:17:18 +0100

    Created a datastore test module so it can be imported and tested anywhere
    Added LaunchVviewModelTest - TODO dependencies

---
+++ launch/launch-datasource-test/src/main/java/com/seancoyle/launch_datasource_test/cache/launch/FakeLaunchDatabase.kt
@@ -0,0 +1,10 @@
+package com.seancoyle.launch_datasource_test.cache.launch
+
+import com.seancoyle.launch_models.model.launch.LaunchModel
+
+class FakeLaunchDatabase {
+
+    // fake for launch table in local db
+    val launchList = mutableListOf<LaunchModel>()
+
+}
