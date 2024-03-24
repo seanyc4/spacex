f0b1c408250aa7cbb2405144ec0055ff6865a89d
asos-seancoyle <sean.coyle@asos.com>
2024-03-24 17:37:33 +0000

    clean up DataResult

--- core/src/main/java/com/seancoyle/core/data/DataResult.kt
+++ core/src/main/java/com/seancoyle/core/data/DataResult.kt
@@ -1,6 +1,6 @@
 package com.seancoyle.core.data
 
-import com.seancoyle.core.data.DataResult.Companion.UNKNOWN_ERROR
+import com.seancoyle.core.data.NetworkErrors.UNKNOWN_NETWORK_ERROR
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
@@ -10,10 +10,6 @@ sealed interface DataResult<out T> {
     data class Success<T>(val data: T) : DataResult<T>
     data class Error(val exception: String) : DataResult<Nothing>
     data object Loading : DataResult<Nothing>
-
-    companion object {
-        const val UNKNOWN_ERROR = "Unknown error"
-    }
 }
 
 fun <T> Flow<T>.asResult(): Flow<DataResult<T>> {
@@ -23,5 +19,5 @@ fun <T> Flow<T>.asResult(): Flow<DataResult<T>> {
         }
         .onStart { emit(DataResult.Loading) }
         .catch { exception ->
-            emit(DataResult.Error(exception.message ?: UNKNOWN_ERROR)) }
+            emit(DataResult.Error(exception.message ?: UNKNOWN_NETWORK_ERROR)) }
 }
