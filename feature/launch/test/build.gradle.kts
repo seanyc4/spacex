f023f882f3f0374aae96ba197e15f78c2b4ce047
asos-seancoyle <sean.coyle@asos.com>
2024-04-14 12:07:01 +0100

    add :feature:launch:test module

---
+++ feature/launch/test/build.gradle.kts
@@ -0,0 +1,16 @@
+apply {
+    from("$rootDir/android-base.gradle")
+    from("$rootDir/hilt.gradle")
+}
+
+plugins {
+    id(libs.plugins.androidLibrary.get().pluginId)
+}
+
+android {
+    namespace = "com.seancoyle.feature.launch.test"
+}
+
+dependencies {
+    implementation(projects.feature.launch.api)
+}
