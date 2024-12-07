c78837413161c71a7f6072b61c9b090d84f57e96
Sean <seancoyle@protonmail.com>
2024-12-07 11:11:08 +0000

    bumps AGP, FireBase BOM

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -1,6 +1,6 @@
 object Android {
     const val appId = "com.seancoyle.spacex"
-    const val compileSdk = 34
+    const val compileSdk = 35
     const val minSdk = 26
     const val targetSdk = 34
     const val versionCode = 1
