7b96c7a8fa7983512d0f77145fc280c002b7ab3e
Sean <seancoyle@protonmail.com>
2025-02-07 21:43:13 +0000

    removes mapper from LaunchNetworkDataSource

--- feature/launch/implementation/src/main/java/com/seancoyle/feature/launch/implementation/data/network/LaunchNetworkDataSourceImpl.kt
+++ feature/launch/implementation/src/main/java/com/seancoyle/feature/launch/implementation/data/network/LaunchNetworkDataSourceImpl.kt
@@ -5,26 +5,24 @@ import com.seancoyle.core.common.di.IODispatcher
 import com.seancoyle.core.common.result.DataError
 import com.seancoyle.core.common.result.Result
 import com.seancoyle.core.data.safeApiCall
-import com.seancoyle.feature.launch.api.domain.model.LaunchTypes
+import com.seancoyle.feature.launch.implementation.data.network.dto.LaunchesDto
 import com.seancoyle.feature.launch.implementation.domain.model.LaunchOptions
 import com.seancoyle.feature.launch.implementation.domain.network.LaunchNetworkDataSource
 import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
 internal class LaunchNetworkDataSourceImpl @Inject constructor(
     private val api: LaunchApiService,
-    private val networkMapper: LaunchNetworkMapper,
     private val crashlytics: Crashlytics,
     @IODispatcher private val ioDispatcher: CoroutineDispatcher
 ) : LaunchNetworkDataSource {
-    override suspend fun getLaunches(launchOptions: LaunchOptions): Result<List<LaunchTypes.Launch>, DataError> {
+
+    override suspend fun getLaunches(launchOptions: LaunchOptions): Result<LaunchesDto, DataError> {
         return safeApiCall(
             dispatcher = ioDispatcher,
             crashlytics = crashlytics
         ) {
-            networkMapper.mapEntityToList(api.getLaunches(launchOptions))
+            api.getLaunches(launchOptions)
         }
     }
 }
--- feature/launch/implementation/src/main/java/com/seancoyle/feature/launch/implementation/domain/network/LaunchNetworkDataSource.kt
+++ feature/launch/implementation/src/main/java/com/seancoyle/feature/launch/implementation/domain/network/LaunchNetworkDataSource.kt
@@ -2,9 +2,9 @@ package com.seancoyle.feature.launch.implementation.domain.network
 
 import com.seancoyle.core.common.result.DataError
 import com.seancoyle.core.common.result.Result
-import com.seancoyle.feature.launch.api.domain.model.LaunchTypes
+import com.seancoyle.feature.launch.implementation.data.network.dto.LaunchesDto
 import com.seancoyle.feature.launch.implementation.domain.model.LaunchOptions
 
 internal interface LaunchNetworkDataSource {
-    suspend fun getLaunches(launchOptions: LaunchOptions): Result<List<LaunchTypes.Launch>, DataError>
+    suspend fun getLaunches(launchOptions: LaunchOptions): Result<LaunchesDto, DataError>
 }
