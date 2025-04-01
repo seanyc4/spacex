0dba55e0776e04ca7c9d127e640f924fc2ce4675
Sean <seancoyle@protonmail.com>
2025-04-01 10:32:40 +0100

    clean up

--- feature/launch/implementation/src/main/java/com/seancoyle/feature/launch/implementation/di/launch/LaunchPreferencesDataStoreProtoModule.kt
+++ feature/launch/implementation/src/main/java/com/seancoyle/feature/launch/implementation/di/launch/LaunchPreferencesDataStoreProtoModule.kt
@@ -19,7 +19,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class LaunchPreferencesDataStoreProtoModule {
+internal class LaunchPreferencesDataStoreProtoModule {
 
     @Singleton
     @Provides
