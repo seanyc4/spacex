f908264255858d9f4474b974466086d953afcf4c
Sean <seancoyle@protonmail.com>
2025-04-03 09:55:38 +0100

    bump version code

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -3,6 +3,6 @@ object Android {
     const val compileSdk = 35
     const val minSdk = 26
     const val targetSdk = 34
-    const val versionCode = 1
+    const val versionCode = 7
     const val versionName = "1.0.0"
 }
