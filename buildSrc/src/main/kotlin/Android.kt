8577c14a24a1ab62fc4deababbebaac9ecc3eda4
Sean <seancoyle@protonmail.com>
2025-04-04 09:39:30 +0100

    update version bump logic

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -3,6 +3,6 @@ object Android {
     const val compileSdk = 35
     const val minSdk = 26
     const val targetSdk = 34
-    const val versionCode = 9
-    const val versionName = "1.9.0"
+    const val versionCode = 8
+    const val versionName = "1.8.0"
 }
