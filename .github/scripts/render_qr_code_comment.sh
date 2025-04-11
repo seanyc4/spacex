27674269694785a680ab03bbc5586638a7ba249d
Sean <seancoyle@protonmail.com>
2025-04-11 16:56:02 +0100

    clean up

--- .github/scripts/render_qr_code_comment.sh
+++ .github/scripts/render_qr_code_comment.sh
@@ -4,13 +4,13 @@ set -e
 TEMPLATE_FILE=".github/templates/qr_code_comment.md"
 RENDERED_FILE=".github/templates/rendered_comment.md"
 
-# Inputs
-ARTIFACT_URL="$1"
-ARTIFACT_URL_ENCODED="$2"
-VERSION="$3"
-DATETIME="$4"
+# Inputs (sanitize to remove whitespace or newlines)
+ARTIFACT_URL="$(echo "$1" | tr -d '\n\r' | xargs)"
+ARTIFACT_URL_ENCODED="$(echo "$2" | tr -d '\n\r' | xargs)"
+VERSION="$(echo "$3" | xargs)"
+DATETIME="$(echo "$4" | xargs)"
 
-# Render the markdown with simple replacement
+# Render the markdown with placeholder replacement
 sed \
   -e "s|{{artifact_url}}|$ARTIFACT_URL|g" \
   -e "s|{{artifact_url_encoded}}|$ARTIFACT_URL_ENCODED|g" \
@@ -18,4 +18,5 @@ sed \
   -e "s|{{datetime}}|$DATETIME|g" \
   "$TEMPLATE_FILE" > "$RENDERED_FILE"
 
+# Output path for use in workflow
 echo "comment_file=$RENDERED_FILE" >> "$GITHUB_OUTPUT"
