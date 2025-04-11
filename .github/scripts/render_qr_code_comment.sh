568c8c63fb3342bd4ec0c273d4744bf0bb298961
Sean <seancoyle@protonmail.com>
2025-04-11 16:56:02 +0100

    clean up

--- .github/scripts/render_qr_code_comment.sh
+++ .github/scripts/render_qr_code_comment.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-TEMPLATE_FILE=".github/templates/qr_code_comment.md"
+TEMPLATE_FILE=".github/templates/qr_comment.md"
 RENDERED_FILE=".github/templates/rendered_comment.md"
 
-# Inputs
-ARTIFACT_URL="$1"
-ARTIFACT_URL_ENCODED="$2"
-VERSION="$3"
-DATETIME="$4"
+# Strip leading/trailing spaces just in case
+ARTIFACT_URL=$(echo "$1" | xargs)
+ARTIFACT_URL_ENCODED=$(echo "$2" | xargs)
+VERSION=$(echo "$3" | xargs)
+DATETIME=$(echo "$4" | xargs)
 
-# Render the markdown with simple replacement
+# Render the template
 sed \
   -e "s|{{artifact_url}}|$ARTIFACT_URL|g" \
   -e "s|{{artifact_url_encoded}}|$ARTIFACT_URL_ENCODED|g" \
