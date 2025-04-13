0677f5bd94db4317429fc10e709e4ad9763ab495
Sean <seancoyle@protonmail.com>
2025-04-13 21:31:04 +0100

    adds assemble_release_apk.sh

--- .github/scripts/assemble_debug_apk.sh
+++ .github/scripts/assemble_debug_apk.sh
@@ -4,7 +4,7 @@ set -e
 APK_NAME=$1
 
 # Build Debug APK
-./gradlew :app:assembleDebug
+./gradlew :app:assembleDebug --stacktrace
 
 # Rename APK script
 ./.github/scripts/rename_artifact.sh "app/build/outputs/apk/debug/app-debug.apk" "app/build/outputs/apk/debug/$APK_NAME"
--- .github/scripts/rename_release_apk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-APP_VERSION=$1
-mv app/build/outputs/apk/release/app-release-unsigned-signed.apk \
-   app/build/outputs/apk/release/spacexDev-release_"$APP_VERSION".apk
