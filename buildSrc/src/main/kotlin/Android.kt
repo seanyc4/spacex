84d822fca534438200159de83598fc81fdae820d
seanyc4 <51095235+seanyc4@users.noreply.github.com>
2025-04-20 15:28:26 +0000

    chore: bump version to 1.41.0

--- buildSrc/src/main/kotlin/Android.kt
+++ buildSrc/src/main/kotlin/Android.kt
@@ -3,6 +3,6 @@ object Android {
     const val compileSdk = 35
     const val minSdk = 26
     const val targetSdk = 34
-    const val versionCode = 40
-    const val versionName = "1.40.0"
+    const val versionCode = 41
+    const val versionName = "1.41.0"
 }
