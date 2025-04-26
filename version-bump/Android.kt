d854b0f36d839c098ec35e55b0db1876e23811a9
Sean <seancoyle@protonmail.com>
2025-04-26 08:37:31 +0100

    Adjusts version bump upload logic

--- version-bump/Android.kt
+++
@@ -1,8 +0,0 @@
-object Android {
-    const val appId = "com.seancoyle.spacex"
-    const val compileSdk = 35
-    const val minSdk = 26
-    const val targetSdk = 34
-    const val versionCode = 53
-    const val versionName = "1.53.0"
-}
