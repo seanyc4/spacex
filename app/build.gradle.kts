47619b49f98752c6c5e1f5bcea316b5196d905bc
seanyc4 <seancoyle@protonmail.com>
2025-10-18 08:03:54 +0100

    removes duplicate gradle entry

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,10 +46,6 @@ android {
         }
     }
 
-    buildFeatures {
-        compose = true
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
