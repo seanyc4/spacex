15d9c3a93d5eb4214e2f2868465915d959e439bb
seanyc4 <seancoyle@protonmail.com>
2025-10-20 09:40:21 +0100

    remove profile check

--- .github/scripts/assemble_release_aab.sh
+++ .github/scripts/assemble_release_aab.sh
@@ -4,13 +4,3 @@ set -e
 # Build Release AAB with Baseline Profile
 echo "Generating Baseline Profile and building Release AAB"
 ./gradlew :app:generateBaselineProfile :app:bundleRelease --stacktrace
-
-# Verify that the Baseline Profile is included in the AAB
-echo "🔍 Verifying baseline profile inclusion in the AAB"
-unzip -l app/build/outputs/bundle/release/app-release.aab | grep baseline-prof.txt || {
-  echo "❌ Baseline profile not found in AAB!"
-  exit 1
-}
-
-echo "✅ Baseline profile found and included in the AAB."
-
