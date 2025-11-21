9fe8e46a886eb78e88cca396799cd3476821d131
seanyc4 <seancoyle@protonmail.com>
2025-11-21 07:52:43 +0000

    increases default timeout from 5 to 10 seconds

--- benchmark/src/main/java/com/seancoyle/benchmark/BenchmarkConstants.kt
+++ benchmark/src/main/java/com/seancoyle/benchmark/BenchmarkConstants.kt
@@ -3,5 +3,5 @@ package com.seancoyle.benchmark
 object BenchmarkConstants {
     const val SPACEX = "com.seancoyle.spacex"
     const val DEFAULT_ITERATIONS = 10
-    const val DEFAULT_TIMEOUT = 5000L
+    const val DEFAULT_TIMEOUT = 10000L
 }
