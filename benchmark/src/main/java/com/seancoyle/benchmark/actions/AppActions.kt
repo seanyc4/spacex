c55070611f288559da24b00b55807bd1a1f41721
seanyc4 <seancoyle@protonmail.com>
2025-11-26 16:07:58 +0000

    Updates Actions & Utils

--- benchmark/src/main/java/com/seancoyle/benchmark/actions/AppActions.kt
+++ benchmark/src/main/java/com/seancoyle/benchmark/actions/AppActions.kt
@@ -1,14 +1,17 @@
 package com.seancoyle.benchmark.actions
 
 import androidx.test.uiautomator.Direction
-import androidx.test.uiautomator.ElementNotFoundException
 import androidx.test.uiautomator.UiAutomatorTestScope
+import com.seancoyle.benchmark.BenchmarkConstants.DEFAULT_TIMEOUT
+
+private const val INDEX = 1
 
 internal fun UiAutomatorTestScope.scrollVerticalView(
     repeatDown: Int = 3,
     repeatUp: Int = 3,
     id: String = ""
 ) {
+    waitForStableInActiveWindow()
     repeat(repeatDown) {
         findScrollableElement(id).fling(Direction.DOWN)
     }
@@ -21,6 +24,7 @@ internal fun UiAutomatorTestScope.scrollHorizontalView(
     repeat: Int = 3,
     id: String = ""
 ) {
+    waitForStableInActiveWindow()
     repeat(repeat) {
         val element = findScrollableElement(id)
         element.fling(Direction.RIGHT)
@@ -28,9 +32,11 @@ internal fun UiAutomatorTestScope.scrollHorizontalView(
     }
 }
 
-private fun UiAutomatorTestScope.findScrollableElement(id: String) =
-    if (id.isNotEmpty()) {
-        onElementOrNull { isScrollable && contentDescription == id }
-    } else {
-        onElementOrNull { isScrollable }
-    } ?: throw ElementNotFoundException("Scrollable element with id: $id not found")
+internal fun UiAutomatorTestScope.clickFirstChildInView(id: String) {
+    val view = findObjectByRes(id)
+    device.waitForIdle(DEFAULT_TIMEOUT)
+    val firstChild = view.children.getOrNull(INDEX) // index 1 to avoid clicking on headers
+    firstChild?.click()
+    device.waitForIdle(DEFAULT_TIMEOUT)
+}
+
