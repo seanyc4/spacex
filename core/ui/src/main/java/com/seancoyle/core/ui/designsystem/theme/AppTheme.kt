c32e87582015075c8294aca36c176a820b500fa7
seanyc4 <seancoyle@protonmail.com>
2025-12-24 18:22:41 +0000

    WIP refactor colors class

--- core/ui/src/main/java/com/seancoyle/core/ui/designsystem/theme/AppTheme.kt
+++ core/ui/src/main/java/com/seancoyle/core/ui/designsystem/theme/AppTheme.kt
@@ -1,106 +1,25 @@
 package com.seancoyle.core.ui.designsystem.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.unit.dp
 
-@ExperimentalMaterialApi
 @Composable
 fun AppTheme(
     isDarkTheme: Boolean = isSystemInDarkTheme(),
-    androidTheme: Boolean = false,
     content: @Composable () -> Unit,
 ) {
-    val colorPalette = if (isDarkTheme) DarkColorPalette else LightColorPalette
-
-    val defaultTheme = AppTheme(
-        color = colorPalette.background,
-        tonalElevation = 2.dp,
-    )
-    val appTheme = when {
-        androidTheme -> if (isDarkTheme) DarkAndroidTheme else LightAndroidTheme
-        else -> defaultTheme
-    }
+    val colorPalette = if (isDarkTheme) darkPalette else lightPalette
 
     CompositionLocalProvider(
-        LocalAppTheme provides appTheme,
+        LocalPalette provides colorPalette,
     ) {
         MaterialTheme(
-            colorScheme = colorPalette,
+            colorScheme = colorPalette.colourScheme,
             typography = AppTypography,
+            shapes = MaterialTheme.shapes,
             content = content
         )
     }
 }
-
-val LightAndroidTheme = AppTheme(color = md_theme_light_background)
-val DarkAndroidTheme = AppTheme(color = md_theme_dark_background)
-
-private val LightColorPalette = lightColorScheme(
-    primary = md_theme_light_primary,
-    onPrimary = md_theme_light_onPrimary,
-    primaryContainer = md_theme_light_primaryContainer,
-    onPrimaryContainer = md_theme_light_onPrimaryContainer,
-    secondary = md_theme_light_secondary,
-    onSecondary = md_theme_light_onSecondary,
-    secondaryContainer = md_theme_light_secondaryContainer,
-    onSecondaryContainer = md_theme_light_onSecondaryContainer,
-    tertiary = md_theme_light_tertiary,
-    onTertiary = md_theme_light_onTertiary,
-    tertiaryContainer = md_theme_light_tertiaryContainer,
-    onTertiaryContainer = md_theme_light_onTertiaryContainer,
-    error = md_theme_light_error,
-    errorContainer = md_theme_light_errorContainer,
-    onError = md_theme_light_onError,
-    onErrorContainer = md_theme_light_onErrorContainer,
-    background = md_theme_light_background,
-    onBackground = md_theme_light_onBackground,
-    surface = md_theme_light_surface,
-    onSurface = md_theme_light_onSurface,
-    surfaceVariant = md_theme_light_surfaceVariant,
-    onSurfaceVariant = md_theme_light_onSurfaceVariant,
-    outline = md_theme_light_outline,
-    outlineVariant = md_theme_light_outlineVariant,
-    inverseOnSurface = md_theme_light_inverseOnSurface,
-    inverseSurface = md_theme_light_inverseSurface,
-    inversePrimary = md_theme_light_inversePrimary,
-    surfaceTint = md_theme_light_surfaceTint,
-    scrim = md_theme_light_scrim
-)
-
-val DarkColorPalette = darkColorScheme(
-    primary = md_theme_dark_primary,
-    onPrimary = md_theme_dark_onPrimary,
-    primaryContainer = md_theme_dark_primaryContainer,
-    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
-    secondary = md_theme_dark_secondary,
-    onSecondary = md_theme_dark_onSecondary,
-    secondaryContainer = md_theme_dark_secondaryContainer,
-    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
-    tertiary = md_theme_dark_tertiary,
-    onTertiary = md_theme_dark_onTertiary,
-    tertiaryContainer = md_theme_dark_tertiaryContainer,
-    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
-    error = md_theme_dark_error,
-    errorContainer = md_theme_dark_errorContainer,
-    onError = md_theme_dark_onError,
-    onErrorContainer = md_theme_dark_onErrorContainer,
-    background = md_theme_dark_background,
-    onBackground = md_theme_dark_onBackground,
-    surface = md_theme_dark_surface,
-    onSurface = md_theme_dark_onSurface,
-    surfaceVariant = md_theme_dark_surfaceVariant,
-    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
-    outline = md_theme_dark_outline,
-    outlineVariant = md_theme_dark_outlineVariant,
-    inverseOnSurface = md_theme_dark_inverseOnSurface,
-    inverseSurface = md_theme_dark_inverseSurface,
-    inversePrimary = md_theme_dark_inversePrimary,
-    surfaceTint = md_theme_dark_surfaceTint,
-    scrim = md_theme_dark_scrim
-)
