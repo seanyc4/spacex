1ba6481efb00e07581663275b5d5186c66c08b7f
seanyc4 <seancoyle@protonmail.com>
2025-12-27 16:03:58 +0000

    WIP removes unused LaunchesUiState

--- feature/launch/implementation/src/main/java/com/seancoyle/feature/launch/implementation/presentation/state/LaunchState.kt
+++ feature/launch/implementation/src/main/java/com/seancoyle/feature/launch/implementation/presentation/state/LaunchState.kt
@@ -3,23 +3,8 @@ package com.seancoyle.feature.launch.implementation.presentation.state
 import android.os.Parcelable
 import androidx.compose.runtime.Stable
 import com.seancoyle.core.domain.Order
-import com.seancoyle.core.ui.NotificationState
 import com.seancoyle.feature.launch.implementation.domain.model.LaunchStatus
-import com.seancoyle.feature.launch.implementation.presentation.model.LaunchUi
 import kotlinx.parcelize.Parcelize
-
-internal sealed interface LaunchesUiState {
-    @Stable
-    data class Success(
-        val launches: List<LaunchUi>
-    ) : LaunchesUiState
-
-    data object Loading : LaunchesUiState
-
-    data class Error(
-        val errorNotificationState: NotificationState? = null
-    ) : LaunchesUiState
-}
 
 @Parcelize
 @Stable
