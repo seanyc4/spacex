9e3c401120c6f0f9e86072bb7285d8824eba46c8
seanyc4 <seancoyle@protonmail.com>
2026-01-16 21:33:51 +0000

    updates baseline profiles

--- benchmark/src/main/kotlin/com/seancoyle/benchmark/actions/MacroBenchmarkUtils.kt
+++ benchmark/src/main/kotlin/com/seancoyle/benchmark/actions/MacroBenchmarkUtils.kt
@@ -3,26 +3,27 @@ package com.seancoyle.benchmark.actions
 import androidx.test.uiautomator.ElementNotFoundException
 import androidx.test.uiautomator.UiAutomatorTestScope
 import androidx.test.uiautomator.textAsString
+import com.seancoyle.benchmark.BenchmarkConstants.DEFAULT_TIMEOUT
 import com.seancoyle.benchmark.BenchmarkConstants.ORBITAL
 
 internal fun UiAutomatorTestScope.findScrollableElement() = onElement { isScrollable }
 
 internal fun UiAutomatorTestScope.findScrollableElement(testTag: String) =
-    onElementOrNull { isScrollable && viewIdResourceName == testTag }
+    onElementOrNull(DEFAULT_TIMEOUT) { isScrollable && viewIdResourceName == testTag }
         ?: throw ElementNotFoundException("Scrollable element with id: $testTag not found")
 
 internal fun UiAutomatorTestScope.findElementByText(textString: String) =
-    onElementOrNull { textAsString() == textString }
+    onElementOrNull(DEFAULT_TIMEOUT) { textAsString() == textString }
         ?: throw ElementNotFoundException("Element with text: $textString not found")
 
 internal fun UiAutomatorTestScope.findElementByRes(resIdString: String) =
-    onElementOrNull { viewIdResourceName == "$ORBITAL:id/$resIdString" }
+    onElementOrNull(DEFAULT_TIMEOUT) { viewIdResourceName == "$ORBITAL:id/$resIdString" }
         ?: throw ElementNotFoundException("Could not find object with resource ID: $ORBITAL:id/$resIdString")
 
 internal fun UiAutomatorTestScope.findElementContainsRes(resIdString: String) =
-    onElementOrNull { viewIdResourceName?.contains(resIdString) == true }
+    onElementOrNull(DEFAULT_TIMEOUT) { viewIdResourceName?.contains(resIdString) == true }
         ?: throw ElementNotFoundException("Could not find object with resource ID:$resIdString")
 
 internal fun UiAutomatorTestScope.findElementByTestTag(testTag: String) =
-    onElementOrNull { viewIdResourceName == testTag }
+    onElementOrNull(DEFAULT_TIMEOUT) { viewIdResourceName == testTag }
         ?: throw ElementNotFoundException("Could not find object with resource ID: $testTag")
