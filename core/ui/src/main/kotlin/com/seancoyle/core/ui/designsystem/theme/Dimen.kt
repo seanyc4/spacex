bdc35a2c5734960624610075c3f0d533fbb7f643
seanyc4 <seancoyle@protonmail.com>
2026-01-21 19:33:34 +0000

    card height increased from 210 to 220

--- core/ui/src/main/kotlin/com/seancoyle/core/ui/designsystem/theme/Dimen.kt
+++ core/ui/src/main/kotlin/com/seancoyle/core/ui/designsystem/theme/Dimen.kt
@@ -26,7 +26,7 @@ object Dimens {
     val toolbarHeight = 54.dp
     val toolbarHeightExpanded = 110.dp
 
-    val launchCardHeight = 210.dp
+    val launchCardHeight = 220.dp
     val launchImageHeight = 220.dp
     val rocketIconSize = 125.dp
 
